dd046630da7778bb2698d9a2caf8729b461e515b
iKath <42527047+iKath@users.noreply.github.com>
2023-08-06 11:40:30 +0800

    Create notability.js

---
+++ Script/notability.js
@@ -0,0 +1,45 @@
+/*************************************
+
+项目名称：Notability
+下载地址：https://t.cn/A6Cgjtei
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/notability\.com\/(global|subscriptions) url script-response-body https://raw.githubusercontent.com/kath/Rewrite/main/notability.js
+
+[mitm]
+hostname = notability.com
+
+*************************************/
+
+
+var kath = JSON.parse($response.body);
+
+kath = {
+   "data" : {
+     "processAppleReceipt" : {
+       "error" : 0,
+       "subscription" : {
+         "productId" : "com.gingerlabs.Notability.premium_subscription",
+         "originalTransactionId" : "570001184068302",
+         "tier" : "premium",
+         "refundedDate" : null,
+         "refundedReason" : null,
+         "isInBillingRetryPeriod" : false,
+         "expirationDate" : "2099-09-09T09:09:09.000Z",
+         "gracePeriodExpiresAt" : null,
+         "overDeviceLimit" : false,
+         "expirationIntent" : null,
+         "__typename" : "AppStoreSubscription",
+         "user" : null,
+         "status" : "canceled",
+         "originalPurchaseDate" : "2022-09-09T09:09:09.000Z"
+       },
+       "__typename" : "SubscriptionResult"
+    }
+  }
+};
+
+$done({body : JSON.stringify(kath)});
